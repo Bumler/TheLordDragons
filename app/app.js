14541df71cc493c7f28268b8dbd984b0b99b87a6
Bumler <hbulmer@callutheran.edu>
2017-03-13 19:23:36 -0700

    Initial commit after nuking repo
    Should be everything but the node modules

---
+++ app/app.js
@@ -0,0 +1,50 @@
+var lordDragonApp = angular.module('lordDragonApp', [])
+
+// LordDragonApp.config(function(){
+// 	//anything that needs to happen before the app runs
+// });
+
+// LordDragonApp.run(function(){
+// 	//fires at run-time
+// });
+
+.controller('lordDragonController', ['$scope', function($scope){
+
+	$scope.selectDragon = function(dragon){
+		$scope.selectedDragon.selected = true;
+		$scope.selectedDragon.name = dragon.name;
+		$scope.selectedDragon.icon = dragon.icon;
+		$scope.selectedDragon.splash = dragon.splash;
+	}
+
+	$scope.dragons = [
+			{
+				name: "Lord Space Dragon",
+				icon: "../img/LSD_ico.jpg",
+				splash: "../img/LordSpaceDragon.jpg"
+			},
+			{
+				name: "Lord Dark Dragon",
+				icon: "../img/LDD_ico.jpg",
+				splash: "../img/LordDarkDragon.jpg"
+			},
+			{
+				name: "Lord Alchemist Dragon",
+				icon: "../img/LAD_ico.jpg",
+				splash: "../img/LordAlchemistDragon.jpg"
+			},
+			{
+				name: "Lord Frost Dragon",
+				icon: "../img/LFD_ico.jpg",
+				splash: "../img/LordFrostDragon.jpg"
+			}
+		];
+
+	$scope.selectedDragon = {
+		selected: false,
+		name: "LordSpaceDragon",
+		icon: "../img/LSD_ico.jpg",
+		splash: "../img/LordSpaceDragon.jpg"
+	}
+
+}]);
