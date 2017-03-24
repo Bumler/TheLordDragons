1e1bbb976144b108582746d48786320277c5d651
Bumler <hbulmer@callutheran.edu>
2017-03-23 21:19:11 -0700

    Splash image is now a slideshow
    Added in $interval so that the splash switches every 10 seconds. Each
    dragon object now has a position attrivute.

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ var lordDragonApp = angular.module('lordDragonApp', [])
 // 	//fires at run-time
 // });
 
-.controller('lordDragonController', ['$scope', function($scope){
+.controller('lordDragonController', ['$scope','$interval', function($scope, $interval){
 
 	$scope.hover = false;
 
@@ -19,34 +19,49 @@ var lordDragonApp = angular.module('lordDragonApp', [])
 		$scope.selectedDragon.splash = dragon.splash;
 	}
 
+	$scope.selectedDragon = {}
+
 	$scope.dragons = [
 			{
 				name: "Lord Space Dragon",
 				icon: "../img/LSD_ico.jpg",
-				splash: "../img/LordSpaceDragon.jpg"
+				splash: "../img/LordSpaceDragon.jpg",
+				splashPosition: "center center"
 			},
 			{
 				name: "Lord Dark Dragon",
 				icon: "../img/LDD_ico.jpg",
-				splash: "../img/LordDarkDragon.jpg"
+				splash: "../img/LordDarkDragon.jpg",
+				splashPosition: "center center"
 			},
 			{
 				name: "Lord Alchemist Dragon",
 				icon: "../img/LAD_ico.jpg",
-				splash: "../img/LordAlchemistDragon.jpg"
+				splash: "../img/LordAlchemistDragon.jpg",
+				splashPosition: "center 7%"
 			},
 			{
 				name: "Lord Frost Dragon",
 				icon: "../img/LFD_ico.jpg",
-				splash: "../img/LordFrostDragon.jpg"
+				splash: "../img/LordFrostDragon.jpg",
+				splashPosition: "center 15%"
 			}
 		];
 
-	$scope.selectedDragon = {
-		selected: false,
-		name: "Lord Space Dragon",
-		icon: "../img/LSD_ico.jpg",
-		splash: "../img/LordSpaceDragon.jpg"
+	$scope.splashDragon = {	}
+
+	var splashPicker = $interval(function() {
+		var num = Math.floor((Math.random() * $scope.dragons.length) + 0);
+		$scope.selectSplash($scope.dragons[num]);
+	}, 10000)
+
+	$scope.selectSplash = function(dragon){
+		$scope.splashDragon.name = dragon.name;
+		$scope.splashDragon.icon = dragon.icon;
+		$scope.splashDragon.splash = dragon.splash;
+		$scope.splashDragon.splashPosition = dragon.splashPosition;
 	}
 
+	$scope.selectDragon($scope.dragons[0]);
+	$scope.selectSplash($scope.dragons[1]);
 }]);
