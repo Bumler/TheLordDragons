93ee54da21935217a72f845d60dc7574f44faba0
Bumler <hbulmer@callutheran.edu>
2017-03-28 22:30:58 -0700

    Reworked the meet the dragons section to be a carousel
    Need to check on ng-animate and add functionality with the portraits

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var lordDragonApp = angular.module('lordDragonApp', [])
+var lordDragonApp = angular.module('lordDragonApp', ['ui.bootstrap'])
 
 // LordDragonApp.config(function(){
 // 	//anything that needs to happen before the app runs
@@ -18,52 +18,56 @@ var lordDragonApp = angular.module('lordDragonApp', [])
 		$scope.selectedDragon.title = dragon.title;
 		$scope.selectedDragon.icon = dragon.icon;
 		$scope.selectedDragon.splash = dragon.splash;
-	}
+	};
 
-	$scope.selectedDragon = {}
+	$scope.selectedDragon = {};
 
 	$scope.dragons = [
-			{
-				name: "Lord Space Dragon",
-				icon: "../img/LSD_ico.jpg",
-				splash: "../img/LordSpaceDragon.jpg",
-				splashPosition: "center center"
-			},
-			{
-				name: "Lord Dark Dragon",
-				title: "Team Captain",
-				icon: "../img/LDD_ico.jpg",
-				splash: "../img/LordDarkDragon.jpg",
-				splashPosition: "center center"
-			},
-			{
-				name: "Lord Alchemist Dragon",
-				title: "The Demolisher",
-				icon: "../img/LAD_ico.jpg",
-				splash: "../img/LordAlchemistDragon.jpg",
-				splashPosition: "center 7%"
-			},
-			{
-				name: "Lord Frost Dragon",
-				icon: "../img/LFD_ico.jpg",
-				splash: "../img/LordFrostDragon.jpg",
-				splashPosition: "center 15%"
-			}
-		];
+	{
+		name: "Lord Space Dragon",
+		icon: "../img/LSD_ico.jpg",
+		splash: "../img/LordSpaceDragon.jpg",
+		splashPosition: "center center",
+		index: 0
+	},
+	{
+		name: "Lord Dark Dragon",
+		title: "Team Captain",
+		icon: "../img/LDD_ico.jpg",
+		splash: "../img/LordDarkDragon.jpg",
+		splashPosition: "center center",
+		index: 1
+	},
+	{
+		name: "Lord Alchemist Dragon",
+		title: "The Demolisher",
+		icon: "../img/LAD_ico.jpg",
+		splash: "../img/LordAlchemistDragon.jpg",
+		splashPosition: "center 7%",
+		index: 2
+	},
+	{
+		name: "Lord Frost Dragon",
+		icon: "../img/LFD_ico.jpg",
+		splash: "../img/LordFrostDragon.jpg",
+		splashPosition: "center 15%",
+		index: 3
+	}
+	];
 
-	$scope.splashDragon = {	}
+	$scope.splashDragon = {	};
 
 	var splashPicker = $interval(function() {
 		var num = Math.floor((Math.random() * $scope.dragons.length) + 0);
 		$scope.selectSplash($scope.dragons[num]);
-	}, 10000)
+	}, 10000);
 
 	$scope.selectSplash = function(dragon){
 		$scope.splashDragon.name = dragon.name;
 		$scope.splashDragon.icon = dragon.icon;
 		$scope.splashDragon.splash = dragon.splash;
 		$scope.splashDragon.splashPosition = dragon.splashPosition;
-	}
+	};
 
 	$scope.selectDragon($scope.dragons[0]);
 	$scope.selectSplash($scope.dragons[1]);
